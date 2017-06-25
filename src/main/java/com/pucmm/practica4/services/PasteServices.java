76c38a9f656d19416be2658d6ab6aafeb6b0b247
john <20131256@ce.pucmm.edu.do>
2017-06-25 13:25:27 -0400

    PasteServices

---
+++ src/main/java/com/pucmm/practica4/services/PasteServices.java
@@ -0,0 +1,35 @@
+package com.pucmm.practica4.services;
+
+import com.pucmm.practica4.entidades.Usuario;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
+/**
+ * Created by john on 25/06/17.
+ */
+
+public class PasteServices extends GestionDb<Paste>  {
+
+    private static PasteServices instancia;
+    private PasteServices (){
+        super(Paste.class);
+    }
+
+    public static PasteServices getInstancia(){
+        if(instancia==null){
+            instancia = new PasteServices();
+        }
+        return instancia;
+    }
+
+
+    public List getPaste(String titulo){
+        EntityManager entityManager = getEntityManager();
+        Query query= entityManager.createNamedQuery("Usuario.findByUsername");
+        query.setParameter("username", titulo+"%");
+        return query.getResultList();
+    }
+
+}
